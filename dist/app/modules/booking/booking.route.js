9b5096da79c61e2bdb7ca728e15bcd6437e7464a
safimiazi <mohibullamiazi@gmail.com>
2024-06-21 14:14:58 +0600

    first commit

--- dist/app/modules/booking/booking.route.js
+++ dist/app/modules/booking/booking.route.js
@@ -12,6 +12,6 @@ const auth_1 = __importDefault(require("../../middlewares/auth"));
 const user_constant_1 = require("../user/user.constant");
 const router = express_1.default.Router();
 router.post("/", (0, auth_1.default)(user_constant_1.USER_ROLE.user), (0, validationRequest_1.default)(booking_validation_1.bookingValidation.carBookingValidation), booking_controller_1.bookingController.carBook);
-router.get('/', (0, auth_1.default)(user_constant_1.USER_ROLE.admin), booking_controller_1.bookingController.getAllBooking);
+router.get('/', (0, auth_1.default)(user_constant_1.USER_ROLE.user, user_constant_1.USER_ROLE.admin), booking_controller_1.bookingController.getAllBooking);
 router.get('/my-bookings', (0, auth_1.default)(user_constant_1.USER_ROLE.user), (0, auth_1.default)(user_constant_1.USER_ROLE.user), booking_controller_1.bookingController.getUsersBooks);
 exports.bookRoute = router;
--- src/app/modules/booking/booking.route.ts
+++ src/app/modules/booking/booking.route.ts
@@ -7,7 +7,7 @@ import { USER_ROLE } from "../user/user.constant";
 const router = express.Router();
 
 router.post("/", auth(USER_ROLE.user), validationRequest(bookingValidation.carBookingValidation), bookingController.carBook)
-router.get('/',auth(USER_ROLE.admin), bookingController.getAllBooking);
+router.get('/',auth(USER_ROLE.user, USER_ROLE.admin), bookingController.getAllBooking);
 router.get('/my-bookings',auth(USER_ROLE.user), auth(USER_ROLE.user), bookingController.getUsersBooks);
 
 export const bookRoute = router;
